117425473cd4317088679a73d8ea092266741904
Lucky Jain <luckyjain78281@gmail.com>
2023-05-06 20:01:33 +0530

    subscriptions page done

---
+++ src/data/channels.js
@@ -0,0 +1,54 @@
+export const channels = [
+    {
+        name: "TED",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnir58zQQJ-i4ilj4c9wUgS91nOJ5iKZQ1qBDyTt=s176-c-k-c0x00ffffff-no-rj-mo"
+    },
+    {
+        name: "The Slow Mo Guys",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnh0OYxI_bzBzH4aWVohWkF3N6bC2XMgIyErjvvv=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "Binging with Babish",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwni6hn3Dk_yq0QJiBn_8PK1UWQKubKkX7ikUNVOOpg=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "Bon Appétit",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwniA1e_eiH-jzjKQH-OJqWJGz9iak-7JtfPTUxp5=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "The Verge",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwniIcBivsQ69myg4ZTzx5K9fQ47RlvG3UxLHvSl-=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "Vox",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnivNuJxby1NvDAyTz2rRp8CkByZ6GHeEiKgfdG5Vg=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "Linus Tech Tips",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnj6wG9f9eCgNxV4mJ6sJdw5_5eSjjat0z6JNQ=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "MrBeast",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwniE29VHnRZ8RjsKmwK-WJovzTvC3V1jI9vE4pJlRw=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "Game Grumps",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwniQOTL25Vegn2G6K5akB5aZYU7JEmz9cKqRrUjJ=s176-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        name: "The Tonight Show Starring Jimmy Fallon",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnjv3Z7J5Ov2cUgUxhC4Z5k8kziHJl6y90QKgS7d1w=s88-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "Gordon Ramsay",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnjIcWZAh3zTtfo6kqSxxVJvVjJziw_z9Kb22f7cGQ=s88-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "TheEllenShow",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnhN7MzHe0k4JGQk5VbdVtVvchTbncJq2e5x=s88-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "CollegeHumor",
+        iconUrl: "https://yt3.ggpht.com/ytc/AAUvwnh3b9Ok0RLVUoZ7lycDbjP-Zu1Bc6BRm5zF5J8V=s88-c-k-c0x00ffffff-no-rj",
+    },
+]
--- src/pages/subscriptions/index.tsx
+++ src/pages/subscriptions/index.tsx
@@ -1,11 +1,77 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { Avatar, Typography } from '@mui/material';
+import { channels } from '../../data/channels'
+import { videos } from '@/data/videos';
+import VideoCard from '@/components/VideoCard';
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
 
 const Subscriptions: React.FC = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
     return (
         <>
-        <Navbar/>
-        
+            <Navbar />
+            <Box sx={{ maxWidth: "100%", bgcolor: '#000', marginTop: '70px' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons
+                    allowScrollButtonsMobile
+                    aria-label="scrollable force tabs example"
+                >
+                    {channels.map((item, index) => (
+                        <Tab key={item.name + index} sx={{ color: '#fff' }} label={<>
+                            <Avatar src={item.iconUrl} />
+                            <Typography fontSize="13px" color="#fff" sx={{ marginY: '5px', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>{item.name}</Typography>
+                        </>} />
+                    ))}
+
+                </Tabs>
+                {channels.map((item, index) => (
+                    <TabPanel key={item.name + index} value={value} index={index}>
+                        <Box display={"flex"} gap={"10px"} flexWrap={"wrap"} marginTop={'10px'}>
+                            {videos.map((video) => (
+                                <VideoCard key={video.id} video={video} />
+                            ))}
+                        </Box>
+                    </TabPanel>
+                ))}
+            </Box>
         </>
     )
 }
